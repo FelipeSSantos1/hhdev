398400a53159cd9c792364aad71d918f707bc003
Felipe S. Santos <felipe.souzasantos@hingehealth.com>
2024-01-18 17:42:08 -0700

    main.rs config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,62 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+use tauri::{SystemTray, Manager, SystemTrayEvent};
+use tauri_plugin_positioner::{Position, WindowExt};
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 
 fn main() {
+    let tray = SystemTray::new().with_title("HH Dev");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(tray)
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+        
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(
+                &window, 
+                NSVisualEffectMaterial::Menu,
+                Some(NSVisualEffectState::Active),
+                Some(6.0)
+            )
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+        
+            Ok(())
+            })
+        .on_system_tray_event(|app, event| {
+            // This is required to get tray-relative positions to work
+            tauri_plugin_positioner::on_tray_event(app, &event);
+
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let window = app.get_window("main").unwrap();
+                    // TrayLeft is not working as expected
+                    // let _ = window.move_window(Position::TrayLeft);
+                    let _ = window.move_window(Position::TopRight);
+                    // toggle application window
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                },
+                _ => {}
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,12 @@
-import { invoke } from "@tauri-apps/api/tauri";
-import { useState } from "react";
-import "./App.css";
-import Button from "@mui/material/Button";
-import reactLogo from "./assets/react.svg";
+import { Button } from "@mui/material";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="m-0 flex flex-col text-center justify-center">
+      <span className="flex-row">
+        <h1>HH Dev App</h1>
+      </span>
       <Button variant="contained">Hello world</Button>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank" rel="noreferrer">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
